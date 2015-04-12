c8d47a211fab6ee5b1b701b8122d01f659fdf8e2
KFCC <from0506shin@gmail.com>
2015-04-12 16:29:51 +0900

    first commit

--- Count_s/ViewController.swift
+++ Count_s/ViewController.swift
@@ -9,7 +9,50 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet var label:UILabel!
+    var number: Int = 0
+    
+    @IBAction func plus(){
+        number = number+1
+        label.text = "\(number)"
+        moshi()
+    }
+    
+    @IBAction func minus(){
+        number = number-1
+        label.text = "\(number)"
+        moshi()
+    }
+    
+    @IBAction func div(){
+        number = number/number
+        label.text = "\(number)"
+        moshi()
+    }
+    
+    
+    @IBAction func multi(){
+        number = number*number
+        label.text = "\(number)"
+        moshi()
+    }
+    
+    
+    @IBAction func clear(){
+        number = 0
+        label.text = "\(number)"
+        moshi()
+    }
+    
+    func moshi(){
+        if (number>=10){
+            label.textColor = UIColor.redColor()
+        }else{
+            label.textColor = UIColor.blackColor()
+        }
+    }
 
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
